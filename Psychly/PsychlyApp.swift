a8aa0d2ba361f61ce31730030a8c5bfa4d6a02cc
Dhilon Prasad <dhilonprasad@gmail.com>
2026-01-20 11:30:06 -0700

    Initial Commit

---
+++ Psychly/PsychlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PsychlyApp.swift
+//  Psychly
+//
+//  Created by Dhilon Prasad on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct PsychlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PsychlyTests/PsychlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PsychlyTests.swift
+//  PsychlyTests
+//
+//  Created by Dhilon Prasad on 1/20/26.
+//
+
+import Testing
+@testable import Psychly
+
+struct PsychlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
